83b2c0c33013116ed5957c10ef8baac6c7d2ef63
phansiri <phansiri@uw.edu>
2014-10-20 08:55:37 -0700

    added update code for main.css and index.html from original git

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,16 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+document.addEventListener('DOMContentLoaded', function() {
+    var signup = document.getElementById('signup');
+    var state = signup.elements['state'];
+    var idx;
+    var option;
+    for (idx = 0; idx <usStates.length; ++idx) {
+        option = document.createElement('option');
+        option.innerHTML = usStates[idx].name;
+        option.value = usStates[idx].code;
+        state.appendChild(option);
+    }
+
+})
