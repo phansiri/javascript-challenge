f728040f9fd5b6749c3d1e3d80b997a7d44c4d76
phansiri <phansiri@uw.edu>
2014-10-22 23:12:03 -0700

    added dob verification

--- js/signup.js
+++ js/signup.js
@@ -4,25 +4,29 @@
 */
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function() {
+function onReady () {
     //Load the State Select
     var signup = document.getElementById('signup');
     var state = signup.elements['state'];
     var idx;
     var option;
-    for (idx = 0; idx <usStates.length; ++idx) {
+    for (idx = 0; idx < usStates.length; ++idx) {
         option = document.createElement('option');
         option.innerHTML = usStates[idx].name;
         option.value = usStates[idx].code;
         state.appendChild(option);
     }
 
-    //hide and show the Occupation Other Input
+        //hide and show the Occupation Other Input
     var otherPop = document.getElementById('occupation');
     otherPop.addEventListener('change', function() {
-        var  occupationOther = signup.elements['occupationOther'];
-        occupationOther.style.display = 'block';
-    })
+        var occupationOther = signup.elements['occupationOther'];
+        if (otherPop.value == 'other') {
+            occupationOther.style.display = 'block';
+        } else {
+            occupationOther.style.display = 'none';
+        }
+    });
 
     //Confirm the "No Thanks" Button
     var noThank = document.getElementById('cancelButton');
@@ -30,49 +34,176 @@ document.addEventListener('DOMContentLoaded', function() {
         if (window.confirm('Are you sure?')) {
             window.location = 'http://www.google.com';
         }
-    })
-    //validate the form before submit
-    signup.addEventListener('submit', onSubmit);
-})
+    });
 
-//validate each block for values
+    signup.addEventListener('submit', onSubmit);
+}
+
 function onSubmit(evt) {
+    var birthdate = this.elements['birthdate'].value;
+
+    try {
+        var age = calculateAge(birthdate);
+        if (age < 13) {
+            displayMessage('You must be 13 years or older to sign up!', true);
+        } else {
+            displayMessage("", false);
+        }
+    }
+
+    catch(exception) {
+        displayError(exception, true);
+    }
 
     evt.returnValue = validateForm(this);
     if (!evt.returnValue && evt.preventDefault()) {
         evt.preventDefault();
     }
+
+
+
+
     return evt.returnValue;
 }
 
 function validateForm(form) {
-    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate', 'occupationOther'];
     var idx;
     var formValid = true;
     for (idx = 0; idx < requiredFields.length; ++idx) {
         formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
     }
+
     return formValid;
 }
 
 function validateRequiredField(field) {
     var value = field.value.trim();
     var valid = value.length > 0;
+
+
+//    if (!field.value()) {//identify field is the zipcode)) {
+//        valid &= validateZip(this.elements['zip']); //test for zip code authentication
+//    }
+
     if (valid) {
         field.className = 'form-control';
     } else {
         field.className = 'form-control invalid-field';
     }
+
     return valid;
 }
 
-//validate zip code
+//unfinished
 function validateZip(field) {
-    var isValid = /^[0-9]{5}(?:-[0-9]{4})?$/.test(zip);
-    if (isValid) {
-        alert('Valid ZipCode');
-    } else {
-        alert('Invalid ZipCode');
-    }
+    var zipRegExp = new RegExp('^\\D{5}$');
+    var isValid = zipRegExp.test(field);
 
 }
+
+//age calc
+function calculateAge(dob) {
+    return moment().diff(dob, 'years');
+}
+function displayError(exception) {
+    displayMessage(error);
+}
+function displayMessage(message, isError) {
+    var msgElem = document.getElementById('birthdateMessage');
+    msgElem.innerHTML = message;
+    msgElem.className = isError ? 'alert alert-danger' : 'alert alert-success';
+    if (isError) {
+        msgElem.style.display = 'block';
+    } else {
+        msgElem.style.display = 'none';
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', onReady);
+
+
+
+
+
+
+
+
+
+
+
+//document.addEventListener('DOMContentLoaded', function() {
+//    //Load the State Select
+//    var signup = document.getElementById('signup');
+//    var state = signup.elements['state'];
+//    var idx;
+//    var option;
+//    for (idx = 0; idx <usStates.length; ++idx) {
+//        option = document.createElement('option');
+//        option.innerHTML = usStates[idx].name;
+//        option.value = usStates[idx].code;
+//        state.appendChild(option);
+//    }
+//
+//    //hide and show the Occupation Other Input
+//    var otherPop = document.getElementById('occupation');
+//    otherPop.addEventListener('change', function() {
+//        var  occupationOther = signup.elements['occupationOther'];
+//        occupationOther.style.display = 'block';
+//    })
+//
+//    //Confirm the "No Thanks" Button
+//    var noThank = document.getElementById('cancelButton');
+//    noThank.addEventListener('click', function() {
+//        if (window.confirm('Are you sure?')) {
+//            window.location = 'http://www.google.com';
+//        }
+//    })
+//    //validate the form before submit
+//    signup.addEventListener('submit', onSubmit);
+//})
+//
+////validate each block for values
+//function onSubmit(evt) {
+//
+//    evt.returnValue = validateForm(this);
+//    if (!evt.returnValue && evt.preventDefault()) {
+//        evt.preventDefault();
+//    }
+//    return evt.returnValue;
+//}
+//
+//function validateForm(form) {
+//    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+//    var idx;
+//    var formValid = true;
+//    for (idx = 0; idx < requiredFields.length; ++idx) {
+//        formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
+//    }
+//    return formValid;
+//}
+//
+//function validateRequiredField(field) {
+//    var value = field.value.trim();
+//    var valid = value.length > 0;
+//    if (valid) {
+//        field.className = 'form-control';
+//    } else {
+//        field.className = 'form-control invalid-field';
+//    }
+//    return valid;
+//}
+//
+////validate zip code
+//function validateZip(field) {
+//    var isValid = /^[0-9]{5}(?:-[0-9]{4})?$/.test(zip);
+//    if (isValid) {
+//        alert('Valid ZipCode');
+//    } else {
+//        alert('Invalid ZipCode');
+//    }
+//
+//}
